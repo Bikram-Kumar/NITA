bf56c4d4544b0875ab24f1feda10c561119461e3
Bikram Kumar <coolbikram0@gmail.com>
2023-09-07 17:45:10 +0530

    created repo

---
+++ 1st/Basic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+  /* 1. Hello world
+  printf("Hello, World!");
+  return 0;
+  */
+  
+  // 2. Input/Output of Integer, Character, and Floating point number
+  // Add two integers
+    
+  printf("Enter an Integer: ");
+  scanf("%d", &a);
+  /*
+  
+  
+  
+  
+  
+  */
+    
+    
+}
