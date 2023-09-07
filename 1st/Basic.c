9cd2525845f51db4e943f49f9e0269ac5422eab4
Bikram Kumar <coolbikram0@gmail.com>
2023-09-07 21:58:15 +0530

    completed 5th sep basic programs page 2

--- 1st/Basic.c
+++ 1st/Basic.c
@@ -2,23 +2,199 @@
 
 int main()
 {
-  /* 1. Hello world
-  printf("Hello, World!");
-  return 0;
-  */
-  
-  // 2. Input/Output of Integer, Character, and Floating point number
-  // Add two integers
-    
-  printf("Enter an Integer: ");
-  scanf("%d", &a);
-  /*
-  
-  
-  
-  
-  
-  */
-    
-    
+	// 1. Hello world
+	// printf("Hello, World!");
+	// return 0;
+	
+
+	/********************************************************************************/
+
+	// 2. Input/Output of Integer, Character, and Floating point number
+	// Add two integers
+
+
+	// int i, i1, i2;
+	// char c;
+	// float f;
+	// printf("Enter an Integer: ");
+	// scanf("%d", &i);
+	// printf("Enter a Character: ");
+	// scanf(" %c", &c);
+	// printf("Enter a Floating point number: ");
+	// scanf("%f", &f);
+	// printf("Enter two integers to add (separated by a comma): ");
+	// scanf("%d, %d", &i1, &i2);
+
+
+	// printf("The entered integer is %d \n", i);
+	// printf("The entered character is %c \n", c);
+	// printf("The entered floating point number is %f \n", f);
+	// printf("The sum of the two integers given (%d and %d) is %d \n", i1, i2, i1+i2);
+
+	
+
+	/********************************************************************************/
+
+	// 3. Multiply two floating point numbers
+
+	// float f1, f2;
+	// printf("Enter two floating point numbers to multiply (separated by a comma): ");
+	// scanf("%f, %f", &f1, &f2);
+	// printf("The product of the given two floating point numbers (%f and %f) is %f", f1, f2, f1*f2);
+
+
+	/********************************************************************************/
+
+	// 4. Find ASCII Value of a character
+
+	// char c;
+	// printf("Enter character: ");
+	// scanf("%c", &c);
+	// printf("%c's ASCII value is %d", c, c);
+
+
+
+
+
+
+	/********************************************************************************/
+
+
+	// 5. Compute quotient and remainder
+
+	// int D, d, q, r;
+
+	// printf("Enter Dividend: ");
+	// scanf("%d", &D);
+	// printf("Enter Divisor: ");
+	// scanf("%d", &d);
+
+	// printf("Quotient: %d \nRemainder: %d", D/d, D % d);
+
+
+
+
+
+
+
+
+	/********************************************************************************/
+
+
+	// 6. Find the size of int, float, double, char
+
+	// printf("The sizes of int, float, double, and char are %d, %d, %d, and %d bytes respectively.",
+	//  		sizeof(int), sizeof(float), sizeof(double), sizeof(char));
+
+
+
+
+	/********************************************************************************/
+
+	// 7. Swap two numbers using and without using third variable
+
+	// int a = 6, b = 8;
+
+	// Using third variable
+
+	// int c;
+
+	// c = a;
+	// a = b;
+	// b = c;
+
+	// Without using third variable
+
+	// a = a + b;
+	// b = a - b;
+	// a = a - b;
+	
+	// printf("%d, %d", a, b);
+
+	
+
+	/********************************************************************************/
+
+	// 8. Find area of a triangle
+
+	// float b, h;
+
+	// printf("Enter base length: ");
+	// scanf("%f", &b);
+	// printf("Enter height: ");
+	// scanf("%f", &h);
+
+	// printf("Area of triangle = %f sq. units", 0.5 * b * h);
+
+
+
+
+	/********************************************************************************/
+
+
+	// 9. Find area of a circle
+
+
+	// float r;
+
+	// printf("Enter radius: ");
+	// scanf("%f", &r);
+
+
+	// printf("Area of circle = %f sq. units", 3.14159265 * r * r);
+
+
+
+	/********************************************************************************/
+
+	// 10. Find area of a square
+
+
+	// float s;
+
+	// printf("Enter side length: ");
+	// scanf("%f", &s);
+
+
+	// printf("Area of square = %f sq. units", s * s);
+
+
+	/********************************************************************************/
+
+	// 11. Find square root of a number using sqrt function
+
+
+	/////////////////////////////////////////////////////////////////////////////////
+	////// ////////////////// /////////// /////////////      ////////////         ///
+	/////// /////// /////// /////////// /// ////////////// ///////////////// ///////
+	///////// /// /// /// /////////// /   // //////////// ///////////////// ///////
+	/////////// /////// //////////// //////// ////////      ////////////// ////////
+	///////////////////////////////////////////////////////////////////////////////
+
+
+
+	/********************************************************************************/
+
+
+	// 12. Calculate gross salary of a person.
+	//     where DA is 12% of basic salary, HR is 7% of basic salary, and TA is 5% of basic salary
+
+	// float base, da, hr, ta, gross;
+	// printf("Enter Base Salary: ");
+	// scanf("%f", &base);
+
+	// da = 0.12 * base;
+	// hr = 0.07 * base;
+	// ta = 0.05 * base;
+
+	// gross = base + da + hr + ta;
+
+	// printf("The Gross Salary is %f", gross);
+
+
+
+
+
+
+	return 0;
 }
