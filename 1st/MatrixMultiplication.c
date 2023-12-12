c1f13af170f6642726253d1d2f48ca17b28abe4a
Bikram Kumar <coolbikram0@gmail.com>
2023-12-12 19:44:11 +0530

    added dimensionality condition

--- 1st/MatrixMultiplication.c
+++ 1st/MatrixMultiplication.c
@@ -3,37 +3,43 @@
 
 int main() {
     
-    int m1, n1, m2, n2;
-    printf("Enter Mat1 size (m1 n1): ");
-    scanf("%d %d", &m1, &n1);
+    int r1, c1, r2, c2;
+    printf("Enter Mat1 size (r1 c1): ");
+    scanf("%d %d", &r1, &c1);
     
-    printf("Enter Mat2 size (m2 n2): ");
-    scanf("%d %d", &m2, &n2);
+    printf("Enter Mat2 size (r2 c2): ");
+    scanf("%d %d", &r2, &c2);
     
-    int mat1[m1][n1];
-    int mat2[m2][n2];
-    int mat3[m1][n2];
+    if (c1 != r2) {
+        printf("Matrices with given dimensions can not be multiplied!\n");
+        printf("(c1 should be equal to r2)\n");
+        return 0;
+    }
     
-    printf("\nEnter Mat1 (%d by %d):\n", m1, n1);
-    for (int i = 0; i < m1; i++) {
-        for (int j = 0; j < n1; j++) {
+    int mat1[r1][c1];
+    int mat2[r2][c2];
+    int mat3[r1][c2];
+    
+    printf("\nEnter Mat1 (%d by %d):\n", r1, c1);
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
             scanf("%d", &mat1[i][j]);
         }
     }
-    printf("\nEnter Mat2 (%d by %d):\n", m2, n2);
-    for (int i = 0; i < m2; i++) {
-        for (int j = 0; j < n2; j++) {
+    printf("\nEnter Mat2 (%d by %d):\n", r2, c2);
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
             scanf("%d", &mat2[i][j]);
         }
     }
     
     printf("\nMat1 * Mat2 = \n");
                 
-    for (int i = 0; i < m1; i++) {
-        for (int j = 0; j < n2; j++) {
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
             mat3[i][j] = 0;
             
-            for (int k = 0; k < n1; k++) {
+            for (int k = 0; k < c1; k++) {
                 mat3[i][j] += mat1[i][k] * mat2[k][j];
             }
             printf("%d ", mat3[i][j]);
