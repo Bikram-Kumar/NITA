0fd102183e0565320ed57edcf47b1036e5c1093c
Bikram Kumar <coolbikram0@gmail.com>
2024-09-28 17:06:51 +0530

    used difftime for type independent computation

--- 3rd/Lab/Timing.c
+++ 3rd/Lab/Timing.c
@@ -3,41 +3,41 @@
 
 #define ITERATIONS 30000
 
-time_t for_time();
-time_t while_time();
-time_t do_while_time();
-time_t goto_time();
-time_t recursion_time();
+double for_time();
+double while_time();
+double do_while_time();
+double goto_time();
+double recursion_time();
 
 
 int main() {
-    time_t f, w, d, g, r;
+    double f, w, d, g, r;
     f = for_time();
     w = while_time();
     d = do_while_time();
     g = goto_time();
     r = recursion_time();
-    printf("Time taken by for: %lld s\n", f);
-    printf("Time taken by while: %lld s\n", w);
-    printf("Time taken by do while: %lld s\n", d);
-    printf("Time taken by goto: %lld s\n", g);
-    printf("Time taken by recursion: %lld s\n", r);
+    printf("Time taken by for: %.2lf s\n", f);
+    printf("Time taken by while: %.2lf s\n", w);
+    printf("Time taken by do while: %.2lf s\n", d);
+    printf("Time taken by goto: %.2lf s\n", g);
+    printf("Time taken by recursion: %.2lf s\n", r);
 
     return 0;
 }
 
 
-time_t for_time () {
+double for_time () {
     time_t t1 = time(NULL);
     for (int i = 0; i < ITERATIONS; i++) {
         printf("Hello World! %d\n", i);
     }
 
-    return (time(NULL) - t1);
+    return (difftime(time(NULL), t1));
 
 }
 
-time_t while_time () {
+double while_time () {
     time_t t1 = time(NULL);
     int i = 0;
     while (i < ITERATIONS) {
@@ -45,10 +45,10 @@ time_t while_time () {
         i++;
     }
 
-    return (time(NULL) - t1);
+    return difftime(time(NULL), t1);
 
 }
-time_t do_while_time () {
+double do_while_time () {
     time_t t1 = time(NULL);
 
     int i = 0;
@@ -59,10 +59,10 @@ time_t do_while_time () {
     } while (i < ITERATIONS);
     
 
-    return (time(NULL) - t1);
+    return difftime(time(NULL), t1);
 
 }
-time_t goto_time () {
+double goto_time () {
     time_t t1 = time(NULL);
 
     int i = 0;
@@ -72,7 +72,7 @@ time_t goto_time () {
         if (i < ITERATIONS) goto loop;
     
 
-    return (time(NULL) - t1);
+    return difftime(time(NULL), t1);
 
 }
 
@@ -83,12 +83,12 @@ void print(int i) {
     }
 }
 
-time_t recursion_time () {
+double recursion_time () {
     time_t t1 = time(NULL);
 
     print(0);
     
 
-    return (time(NULL) - t1);
+    return difftime(time(NULL), t1);
 
 }
