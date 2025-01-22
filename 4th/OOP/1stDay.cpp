0d023ec7b17c9cc378f0ef586ae9e3cca144ab2b
Bikram Kumar <coolbikram0@gmail.com>
2025-01-22 21:26:38 +0530

    1st day part 2

--- 4th/OOP/1stDay.cpp
+++ 4th/OOP/1stDay.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 
 void p1(), p2(), p3(), p4();
+void p5(), p6(), p7(), p8();
 int main () {
-    p1();
-    p2();
-    p3();
-    p4();
+    // p1();
+    // p2();
+    // p3();
+    // p4();
+    p5();
+    p6();
+    p7();
+    p8();
 
     return 0;
 }
@@ -51,4 +57,83 @@ void p4() {
     cout << "Enter three nums: ";
     cin >> a >> b >> c;
     cout << "Max: " << max(a, max(b, c)) << endl;
+}
+
+
+bool isPrime(int n) {
+    if (n == 2) return true;
+    if ((n < 2) || (n % 2 == 0)) return false;
+
+    int s = sqrt(n);
+    for (int i = 3; i <= s; i += 2) {
+        if (isPrime(i) && (n % i == 0)) return false;
+    }
+    return true;
+}
+
+// prime or not
+void p5 () {
+    int n;
+    cout << "Enter num: ";
+    cin >> n;
+    if (isPrime(n)) cout << "Prime" << endl;
+    else cout << "Not Prime" << endl;
+
+}
+
+
+// palindrome check
+void p6 () {
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+    int n = s.size();
+    for (int i = 0; i <= n/2; i++) {
+        if (s[i] != s[n-i-1]) {
+            cout << "Not palindrome" << endl;
+            return;
+        }
+    }
+    cout << "Palindrome" << endl;
+}
+
+
+// armstrong num
+void p7 () {
+    int n;
+    cout << "Enter num: ";
+    cin >> n;
+    int k = n;
+    int sum = 0;
+    while (k != 0) {
+        int r = k % 10;
+        sum += r * r * r;
+        k /= 10;
+    }
+    if (n == sum) cout << n << " is an Armstrong Number" << endl;
+    else cout << n << " is not an Armstrong Number" << endl;
+    
+
+}
+
+
+
+// check vowel or consonant using switch case
+void p8 () {
+    char c;
+    cout << "Enter letter: ";
+    cin >> c;
+    switch (c) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            cout << "Vowel" << endl;
+            break;
+        default:
+            cout << "Consonant" << endl;
+            break;
+    }
+
 }
